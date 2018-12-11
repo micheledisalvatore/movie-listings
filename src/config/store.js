abdbe684f32b2b5326702da2f1592c26e3e2facb
Michele Di Salvatore <github@micheledisalvatore.it>
2018-12-11 13:47:56 -0600

    fix build error

--- src/config/store.js
+++ src/config/store.js
@@ -5,7 +5,7 @@ import reducers from '../reducers';
 import sagas from '../sagas';
 
 /* eslint-disable no-underscore-dangle */
-const reduxDevTools = process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 /* eslint-enable */
 
 const sagaMiddleware = createSagaMiddleware();
@@ -13,9 +13,8 @@ const middleware = applyMiddleware(sagaMiddleware);
 
 export const store = createStore(
   reducers,
-  compose(
+  composeEnhancers(
     middleware,
-    reduxDevTools,
   ),
 );
 
